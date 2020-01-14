12a8f83fb6450644e8eab2607c5cea90c8127a21
cezuppo <czuppo@gmail.com>
2020-01-14 08:00:11 -0300

    Informações do caminho do arquivo.

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,21 +6,12 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		File path = new File(strPath);
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created successfully: " + success);
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
 		sc.close();
 	}
 }
